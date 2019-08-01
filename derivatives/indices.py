d306d23a9a89577dac900b9152b313936f56655b
Jeff Albrecht <geospatialjeff@gmail.com>
2019-07-31 22:56:45 -0700

    better abstraction, storing list of bands on init, adding method to return compatible derivatives for a specific item

---
+++ derivatives/indices.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from derivatives.item import StacItem
+from derivatives.utils import get_methods
+
+
+class BandError(Exception):
+    pass
+
+def requirements(bands):
+    def wrapper(f):
+        def wrapped_f(self):
+            for band in bands:
+                if not hasattr(self, band):
+                    raise BandError("STAC Item {} is missing the '{}' band which is required for {} ({})".format(
+                        self.item['id'],
+                        band,
+                        f.__name__,
+                        bands
+                    ))
+            return f(self)
+        wrapped_f.bands = bands
+        return wrapped_f
+    return wrapper
+
+
+class StacIndices(StacItem):
+
+
+    def __init__(self, item):
+        super().__init__(item)
+
+    def derivatives(self):
+        valids = []
+        methods = [x for x in get_methods(self) if x not in self.base_methods]
+        for method in methods:
+            band_reqs = getattr(getattr(self, method), 'bands')
+            if set(band_reqs).issubset(set(self.bandlist)):
+                valids.append(method)
+        return valids
+
+    @requirements(["red", "nir"])
+    def ndvi(self):
+        red, profile = self.read_band('red', profile=True)
+        nir = self.read_band('nir')
+        num = nir - red
+        den = (nir + red) + 0.00000000001
+        ndvi = np.divide(num, den)
+        return ndvi
+
+    @requirements(["redge", "nir"])
+    def ndre(self):
+        redge, profile = self.read_band('rededge', profile=True)
+        nir = self.read_band('nir')
+        num = nir - redge
+        den = (nir + redge) + 0.00000000001
+        ndre = np.divide(num, den)
+        return ndre
--- derivatives/item.py
+++ derivatives/item.py
@@ -1,5 +1,5 @@
-import numpy as np
 import rasterio
+
 
 # https://github.com/radiantearth/stac-spec/tree/master/extensions/eo#common-band-names
 BAND_NAMES = [
@@ -20,60 +20,28 @@ BAND_NAMES = [
 # TODO: Fix np.seterr, it isn't working propertly for some reason
 # np.seterr(divide='ignore', invalid='ignore')
 
-
-class BandError(Exception):
-    pass
-
-def requirements(bands):
-    def wrapper(f):
-        def wrapped_f(self):
-            for band in bands:
-                if not hasattr(self, band):
-                    raise BandError("STAC Item {} is missing the '{}' band which is required for {} ({})".format(
-                        self.item['id'],
-                        band,
-                        f.__name__,
-                        bands
-                    ))
-            return f(self)
-        return wrapped_f
-    return wrapper
-
 class StacItem(object):
+
+    base_methods = ["__init__", "derivatives", "load_bands", "read_band"]
 
     def __init__(self, item):
         self.item = item
         self.load_bands()
 
     def load_bands(self):
+        bandlist = []
         for asset in self.item['assets']:
             try:
                 band_name = self.item['assets'][asset]['common_name']
                 if band_name in BAND_NAMES:
                     setattr(self, band_name, self.item['assets'][asset])
+                    bandlist.append(band_name)
             except KeyError:
                 pass
+        self.bandlist = bandlist
 
     def read_band(self, band, profile=False):
         with rasterio.open(getattr(self, band)['href']) as src:
             if profile:
                 return [src.read(1).astype(float), profile]
             return src.read(1).astype(float)
-
-    @requirements(["red", "nir"])
-    def ndvi(self):
-        red, profile = self.read_band('red', profile=True)
-        nir = self.read_band('nir')
-        num = nir - red
-        den = (nir + red) + 0.00000000001
-        ndvi = np.divide(num, den)
-        return ndvi
-
-    @requirements(["redge", "nir"])
-    def ndre(self):
-        redge, profile = self.read_band('rededge', profile=True)
-        nir = self.read_band('nir')
-        num = nir - redge
-        den = (nir + redge) + 0.00000000001
-        ndre = np.divide(num, den)
-        return ndre
