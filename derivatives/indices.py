69f7aefc6ed06bc5c5c5428bbd992bfe708834fa
Jeff Albrecht <geospatialjeff@gmail.com>
2019-08-01 11:58:26 -0700

    decorator loads band arrays and generates profile

--- derivatives/indices.py
+++ derivatives/indices.py
@@ -11,7 +11,9 @@ class BandError(Exception):
 def requirements(bands):
     def wrapper(f):
         def wrapped_f(self):
-            for band in bands:
+            args = {}
+            for idx, band in enumerate(bands):
+                # Make sure STAC Item has all required bands to create the indice
                 if not hasattr(self.bands, band):
                     raise BandError("STAC Item {} is missing the '{}' band which is required for {} ({})".format(
                         self.item['id'],
@@ -19,7 +21,14 @@ def requirements(bands):
                         f.__name__,
                         bands
                     ))
-            return f(self)
+                else:
+                    # Read the band and update profile
+                    if idx == 0:
+                        arr, profile = self.read_band(band, profile=True)
+                        profile['dtype'] = 'float32'
+                        args.update({band: arr, 'profile': profile})
+                    args.update({band: self.read_band(band).astype('float32')})
+            return f(self, **args)
         wrapped_f.bands = bands
         return wrapped_f
     return wrapper
@@ -41,21 +50,19 @@ class StacIndices(StacItem):
         return valids
 
     @requirements(["red", "nir"])
-    def ndvi(self):
-        red, profile = self.read_band('red', profile=True)
-        profile['dtype'] = 'float32'
-        nir = self.read_band('nir')
+    def ndvi(self, **kwargs):
+        nir = kwargs.get('nir')
+        red = kwargs.get('red')
         num = nir - red
         den = (nir + red) + 0.0000001
         ndvi = np.divide(num, den)
-        return Outputs(ndvi, profile)
+        return Outputs(ndvi, kwargs.get('profile'))
 
     @requirements(["redge", "nir"])
-    def ndre(self):
-        redge, profile = self.read_band('rededge', profile=True)
-        profile['dtype'] = 'float32'
-        nir = self.read_band('nir')
+    def ndre(self, **kwargs):
+        nir = kwargs.get('nir')
+        redge = kwargs.get('redge')
         num = nir - redge
         den = (nir + redge) + 0.0000001
         ndre = np.divide(num, den)
-        return Outputs(ndre, profile)
+        return Outputs(ndre, kwargs.get('profile'))
