d8ec62325fb390edea4b5f826eb07c89d6609746
Jeff Albrecht <geospatialjeff@gmail.com>
2019-08-04 21:57:40 -0700

    adding common indices: msavi2, avi, savi, osavi, ndmi, ndwi, ndbi, bu, bsi, si, ndsi, ndgi, nbri, and npcri

--- derivatives/indices.py
+++ derivatives/indices.py
@@ -67,3 +67,126 @@ class StacIndices(StacItem):
         den = (nir + redge) + 0.0000001
         ndre = np.divide(num, den)
         return Outputs(ndre, kwargs.get('profile'))
+
+    @requirements(["red", "nir"])
+    def msavi2(self, **kwargs):
+        red = kwargs.get('red')
+        nir = kwargs.get('nir')
+        num = (2 * nir + 1 - np.sqrt((2 * nir + 1)**2 - 8 * (nir - red)))
+        msavi2 = np.clip(np.divide(num, 2), -1, 1)
+        return Outputs(msavi2, kwargs.get('profile'))
+
+    @requirements(["red", "nir"])
+    def avi(self, **kwargs):
+        red = kwargs.get('red')
+        nir = kwargs.get('nir')
+        avi = np.cbrt((nir + 1) * (256 - red) * (nir - red))
+        return Outputs(avi, kwargs.get('profile'))
+
+    @requirements(["red", "nir"])
+    def savi(self, **kwargs):
+        red = kwargs.get('red')
+        nir = kwargs.get('nir')
+        L = kwargs.get('L') if kwargs.get('L') else 0.5
+        num = (nir - red) * (1 + L)
+        den = (nir + red + L)
+        savi = np.clip(np.divide(num, den), -1, 1)
+        return Outputs(savi, kwargs.get('profile'))
+
+    @requirements(["red", "nir"])
+    def osavi(self, **kwargs):
+        red = kwargs.get('red')
+        nir = kwargs.get('nir')
+        num = nir - red
+        den = nir + red + 0.16
+        osavi = np.clip(np.divide(num, den), -1, 1)
+        return Outputs(osavi, kwargs.get('profile'))
+
+    @requirements(["nir", "swir16"])
+    def ndmi(self, **kwargs):
+        # moisture index
+        nir = kwargs.get('nir')
+        swir = kwargs.get('swir16')
+        num = nir - swir
+        den = nir + swir + 0.0000001
+        ndwi = np.divide(num, den)
+        return Outputs(ndwi, kwargs.get('profile'))
+
+    @requirements(["green", "nir"])
+    def ndwi(self, **kwargs):
+        green = kwargs.get('green')
+        nir = kwargs.get('nir')
+        num = green - nir
+        den = green + nir + 0.0000001
+        ndwi = np.divide(num, den)
+        return Outputs(ndwi, kwargs.get('profile'))
+
+    @requirements(["nir", "swir16"])
+    def ndbi(self, **kwargs):
+        nir = kwargs.get('nir')
+        swir = kwargs.get('swir16')
+        num = swir - nir
+        den = swir + nir + 0.0000001
+        ndwi = np.divide(num, den)
+        return Outputs(ndwi, kwargs.get('profile'))
+
+    @requirements(["red", "nir", "swir16"])
+    def bu(self, **kwargs):
+        bu = self.ndbi().to_array() - self.ndvi().to_array()
+        return Outputs(bu, kwargs.get('profile'))
+
+    @requirements(["blue", "red", "nir", "swir16"])
+    def bsi(self, **kwargs):
+        blue = kwargs.get('blue')
+        red = kwargs.get('red')
+        nir = kwargs.get('nir')
+        swir = kwargs.get('swir16')
+        num = (swir + red) - (nir + blue)
+        den = (swir + red) + (nir + blue) + 0.0000001
+        bsi = np.divide(num, den)
+        return Outputs(bsi, kwargs.get('profile'))
+
+    @requirements(["blue", "green", "red"])
+    def si(self, **kwargs):
+        blue = kwargs.get('blue')
+        green = kwargs.get('green')
+        red = kwargs.get('red')
+        si = np.cbrt((1 - blue) * (1 - green) * (1 - red))
+        return Outputs(si, kwargs.get('profile'))
+
+    @requirements(["green", "swir16"])
+    def ndsi(self, **kwargs):
+        green = kwargs.get('green')
+        swir = kwargs.get('swir16')
+        num = green - swir
+        den = green + swir + 0.0000001
+        ndsi = np.divide(num, den)
+        return Outputs(ndsi, kwargs.get('profile'))
+
+    @requirements(["green", "red"])
+    def ndgi(self, **kwargs):
+        green = kwargs.get('green')
+        red = kwargs.get('red')
+        num = green - red
+        den = green + red + 0.0000001
+        ndgi = np.divide(num, den)
+        return Outputs(ndgi, kwargs.get('profile'))
+
+    @requirements(["nir", "swir22"])
+    def nbri(self, **kwargs):
+        nir = kwargs.get('nir')
+        swir = kwargs.get('swir22')
+        num = nir - swir
+        den = nir + swir + 0.0000001
+        nbri = np.divide(num, den)
+        return Outputs(nbri, kwargs.get('profile'))
+
+    @requirements(["red", "blue"])
+    def npcri(self, **kwargs):
+        red = kwargs.get('red')
+        blue = kwargs.get('blue')
+        num = red - blue
+        den = red + blue + 0.0000001
+        npcri = np.divide(num, den)
+        return Outputs(npcri, kwargs.get('profile'))
+
